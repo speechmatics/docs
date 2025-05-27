6a9e974f92e984883eb6d2697c8b64e6d70afdd9
Matt Nemitz <mattn@speechmatics.com>
2025-05-27 23:24:58 +0100

    Unset Docusaurus `code` element defaults

---
+++ src/theme/CodeInline/index.tsx
@@ -0,0 +1,14 @@
+import { Code } from "@radix-ui/themes";
+import type { Props } from "@theme/CodeInline";
+import React, { type ReactNode } from "react";
+
+// Simple component used to render inline code blocks
+// its purpose is to be swizzled and customized
+// MDX 1 used to have a inlineCode comp, see https://mdxjs.com/migrating/v2/
+export default function CodeInline(props: Props): ReactNode {
+  return (
+    <Code variant="outline" color="gray" asChild>
+      <code {...props} style={{ border: "unset" }} />
+    </Code>
+  );
+}
