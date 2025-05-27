4be2cdb744deb96d2295610c4f26706d6a6eb554
Matt Nemitz <mattn@speechmatics.com>
2025-05-27 13:26:00 +0100

    Fix link colors

--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -1,15 +1,131 @@
-import type { WrapperProps } from "@docusaurus/types";
-import { Card } from "@radix-ui/themes";
-import DocCard from "@theme-original/DocCard";
-import type DocCardType from "@theme/DocCard";
+import Link from "@docusaurus/Link";
+import { translate } from "@docusaurus/Translate";
+import isInternalUrl from "@docusaurus/isInternalUrl";
+import {
+  findFirstSidebarItemLink,
+  useDocById,
+} from "@docusaurus/plugin-content-docs/client";
+import { usePluralForm } from "@docusaurus/theme-common";
+import clsx from "clsx";
 import React, { type ReactNode } from "react";
 
-type Props = WrapperProps<typeof DocCardType>;
+import type {
+  PropSidebarItemCategory,
+  PropSidebarItemLink,
+} from "@docusaurus/plugin-content-docs";
+import type { Props } from "@theme/DocCard";
+import Heading from "@theme/Heading";
 
-export default function DocCardWrapper(props: Props): ReactNode {
+import { Card } from "@radix-ui/themes";
+import styles from "./styles.module.css";
+
+function useCategoryItemsPlural() {
+  const { selectMessage } = usePluralForm();
+  return (count: number) =>
+    selectMessage(
+      count,
+      translate(
+        {
+          message: "1 item|{count} items",
+          id: "theme.docs.DocCard.categoryDescription.plurals",
+          description:
+            "The default description for a category card in the generated index about how many items this category includes",
+        },
+        { count },
+      ),
+    );
+}
+
+function CardContainer({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}): ReactNode {
+  return (
+    <Link
+      href={href}
+      className={clsx("card padding--lg", styles.cardContainer)}
+    >
+      {children}
+    </Link>
+  );
+}
+
+function CardLayout({
+  href,
+  icon,
+  title,
+  description,
+}: {
+  href: string;
+  icon: ReactNode;
+  title: string;
+  description?: string;
+}): ReactNode {
   return (
     <Card asChild>
-      <DocCard {...props} />
+      <Link href={href}>
+        <Heading
+          as="h2"
+          className={clsx("text--truncate", styles.cardTitle)}
+          title={title}
+        >
+          {icon} {title}
+        </Heading>
+        {description && (
+          <p
+            className={clsx("text--truncate", styles.cardDescription)}
+            title={description}
+          >
+            {description}
+          </p>
+        )}
+      </Link>
     </Card>
   );
+}
+
+function CardCategory({ item }: { item: PropSidebarItemCategory }): ReactNode {
+  const href = findFirstSidebarItemLink(item);
+  const categoryItemsPlural = useCategoryItemsPlural();
+
+  // Unexpected: categories that don't have a link have been filtered upfront
+  if (!href) {
+    return null;
+  }
+
+  return (
+    <CardLayout
+      href={href}
+      icon="🗃️"
+      title={item.label}
+      description={item.description ?? categoryItemsPlural(item.items.length)}
+    />
+  );
+}
+
+function CardLink({ item }: { item: PropSidebarItemLink }): ReactNode {
+  const icon = isInternalUrl(item.href) ? "📄️" : "🔗";
+  const doc = useDocById(item.docId ?? undefined);
+  return (
+    <CardLayout
+      href={item.href}
+      icon={icon}
+      title={item.label}
+      description={item.description ?? doc?.description}
+    />
+  );
+}
+
+export default function DocCard({ item }: Props): ReactNode {
+  switch (item.type) {
+    case "link":
+      return <CardLink item={item} />;
+    case "category":
+      return <CardCategory item={item} />;
+    default:
+      throw new Error(`unknown item type ${JSON.stringify(item)}`);
+  }
 }
--- src/theme/MDXComponents/A.tsx
+++ src/theme/MDXComponents/A.tsx
@@ -1,15 +1,12 @@
-import type { WrapperProps } from "@docusaurus/types";
-import { Link } from "@radix-ui/themes";
-import A from "@theme-original/MDXComponents/A";
-import type AType from "@theme/MDXComponents/A";
+import DocusaurusLink from "@docusaurus/Link";
+import { Link as RadixLink } from "@radix-ui/themes";
+import type { Props } from "@theme/MDXComponents/A";
 import React, { type ReactNode } from "react";
 
-type Props = WrapperProps<typeof AType>;
-
-export default function AWrapper(props: Props): ReactNode {
+export default function MDXA(props: Props): ReactNode {
   return (
-    <Link asChild>
-      <A {...props} />
-    </Link>
+    <RadixLink asChild>
+      <DocusaurusLink {...props} />
+    </RadixLink>
   );
 }
