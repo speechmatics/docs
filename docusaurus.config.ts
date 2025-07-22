c83de48972f33a7922bccec9d923967243c77723
Tudor Evans <tudore@speechmatics.com>
2025-07-22 13:24:35 +0100

    update algolia index

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -129,7 +129,7 @@ const config: Config = {
       // Public API key: it is safe to commit it
       apiKey: "4192375eb6e5980b954ea3a77a14ff99",
 
-      indexName: "beta_Beta docs",
+      indexName: "prod",
 
       // Optional: see doc section below
       contextualSearch: true,
