2c9ae49de7438e0fd8ed84ad3ec525735ec4c8de
Matt Nemitz <mattn@speechmatics.com>
2025-07-23 12:53:32 +0100

    Fix broken JS RT example (#53)

--- docs/speech-to-text/realtime/assets/javascript-radio-example.js
+++ docs/speech-to-text/realtime/assets/javascript-radio-example.js
@@ -3,59 +3,38 @@ import { createSpeechmaticsJWT } from "@speechmatics/auth";
 import { RealtimeClient } from "@speechmatics/real-time-client";
 
 const apiKey = YOUR_API_KEY;
-
 const client = new RealtimeClient();
-
 const streamURL = "https://media-ice.musicradio.com/LBCUKMP3";
 
-const stream = https.get(streamURL, (response) => {
-  // Handle the response stream
-  response.on("data", (chunk) => {
-    client.sendAudio(chunk);
-  });
-
-  response.on("end", () => {
-    console.log("Stream ended");
-    client.stopRecognition({ noTimeout: true });
-  });
-
-  response.on("error", (error) => {
-    console.error("Stream error:", error);
-    client.stopRecognition();
-  });
-});
-
-stream.on("error", (error) => {
-  console.error("Request error:", error);
-  client.stopRecognition();
-});
-
-client.addEventListener("receiveMessage", ({ data }) => {
-  if (data.message === "AddTranscript") {
-    for (const result of data.results) {
-      if (result.type === "word") {
-        process.stdout.write(" ");
+async function transcribe() {
+  // Print transcript as we receive it
+  client.addEventListener("receiveMessage", ({ data }) => {
+    if (data.message === "AddTranscript") {
+      for (const result of data.results) {
+        if (result.type === "word") {
+          process.stdout.write(" ");
+        }
+        process.stdout.write(`${result.alternatives?.[0].content}`);
+        if (result.is_eos) {
+          process.stdout.write("\n");
+        }
       }
-      process.stdout.write(`${result.alternatives?.[0].content}`);
-      if (result.is_eos) {
-        process.stdout.write("\n");
-      }
+    } else if (data.message === "EndOfTranscript") {
+      process.stdout.write("\n");
+      process.exit(0);
+    } else if (data.message === "Error") {
+      process.stdout.write(`\n${JSON.stringify(data)}\n`);
+      process.exit(1);
     }
-  } else if (data.message === "EndOfTranscript") {
-    process.stdout.write("\n");
-    process.exit(0);
-  } else if (data.message === "Error") {
-    process.stdout.write(`\n${JSON.stringify(data)}\n`);
-    process.exit(1);
-  }
-});
+  });
 
-createSpeechmaticsJWT({
-  type: "rt",
-  apiKey,
-  ttl: 60, // 1 minute
-}).then((jwt) => {
-  client.start(jwt, {
+  const jwt = await createSpeechmaticsJWT({
+    type: "rt",
+    apiKey,
+    ttl: 60, // 1 minute
+  });
+
+  await client.start(jwt, {
     transcription_config: {
       language: "en",
       operating_point: "enhanced",
@@ -65,4 +44,28 @@ createSpeechmaticsJWT({
       },
     },
   });
-});
+
+  const stream = https.get(streamURL, (response) => {
+    // Handle the response stream
+    response.on("data", (chunk) => {
+      client.sendAudio(chunk);
+    });
+
+    response.on("end", () => {
+      console.log("Stream ended");
+      client.stopRecognition({ noTimeout: true });
+    });
+
+    response.on("error", (error) => {
+      console.error("Stream error:", error);
+      client.stopRecognition();
+    });
+  });
+
+  stream.on("error", (error) => {
+    console.error("Request error:", error);
+    client.stopRecognition();
+  });
+}
+
+transcribe();
