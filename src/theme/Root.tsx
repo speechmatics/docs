7984eab62e495f732c349f90d1f3ea703cd756b8
Matt Nemitz <mattn@speechmatics.com>
2025-07-25 15:32:24 +0100

    Format/lint (#58)

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,14 +1,12 @@
 import { Theme } from "@radix-ui/themes";
-import React from "react";
 import MixpanelProvider from "@site/src/components/MixpanelProvider";
+import React from "react";
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
   return (
     <Theme accentColor="cyan">
-      <MixpanelProvider>
-        {children}
-      </MixpanelProvider>
+      <MixpanelProvider>{children}</MixpanelProvider>
     </Theme>
   );
 }
