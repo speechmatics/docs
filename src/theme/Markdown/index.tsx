513cb3ee702ed6d411b6b429167bc45480f810ee
Matt Nemitz <mattn@speechmatics.com>
2025-08-05 17:25:16 +0100

    Comment with issue

--- src/theme/Markdown/index.tsx
+++ src/theme/Markdown/index.tsx
@@ -9,7 +9,7 @@ import remarkAdmonition from "./remark-admonition";
 function Markdown({ children }) {
   return (
     <ReactMarkdown
-      // @ts-ignore type error due to different dependency versions
+      // @ts-ignore type error due to different dependency versions: Only reason for this is this issue: https://github.com/hashicorp/next-mdx-remote/issues/403
       rehypePlugins={[rehypeRaw]}
       remarkPlugins={[remarkGfm, remarkAdmonition]}
       components={{
