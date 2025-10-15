ccfd695d37992501e76e378fb92f5c3bf26b33c0
Daniel Nurkowski <danieln@speechmatics.com>
2025-10-15 13:25:06 +0100

    add batch speaker id docs

--- docs/speech-to-text/batch/sidebar.ts
+++ docs/speech-to-text/batch/sidebar.ts
@@ -23,6 +23,10 @@ export default {
       id: "speech-to-text/batch/batch_diarization",
     },
     {
+      type: "doc",
+      id: "speech-to-text/batch/speaker_identification",
+    },
+    {
       type: "category",
       label: "Speech intelligence",
       items: [
