168a3698069046436fa431094e139e1f776f8bc6
Owais Aamir <aamirt@speechmatics.com>
2025-10-24 15:55:24 +0100

    Update STT helm chart version to 0.7.0

--- sm-variables.ts
+++ sm-variables.ts
@@ -3,7 +3,7 @@ export const smVariables = {
   latestApplianceVersion: "6.2.1",
   latestContainerVersion: "13.0.0", // Make sure to manually update docs/on-prem/containers/sm-inference.yaml
   usageContainerVersion: "0.3.0",
-  helmChartVersion: "0.6.0",
+  helmChartVersion: "0.7.0",
 } as const;
 
 // TODO investigate if there is a better way to handle these version numbers
