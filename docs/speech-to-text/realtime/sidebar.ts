922898de7f1edeb3cd5edd28e134a2ec03a14e3f
J-Jaywalker <103967935+J-Jaywalker@users.noreply.github.com>
2025-11-05 10:34:30 +0000

    Update docs/speech-to-text/realtime/sidebar.ts
    Co-authored-by: Tudor Evans <104087420+TudorCRL@users.noreply.github.com>

--- docs/speech-to-text/realtime/sidebar.ts
+++ docs/speech-to-text/realtime/sidebar.ts
@@ -28,7 +28,7 @@ export default {
     },
     {
       type: "doc",
-      id: "speech-to-text/realtime/end-of-utterance",
+      id: "speech-to-text/realtime/end-of-turn",
     },
     {
       type: "category",
