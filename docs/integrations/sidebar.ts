d091f1662255e44c842f95e30d845625a5bd9153
lgavincrl <lucy.gavin@speechmatics.com>
2025-11-28 12:53:23 +0000

    hopeful fix#1

--- docs/integrations/sidebar.ts
+++ docs/integrations/sidebar.ts
@@ -1,54 +1,30 @@
 export default [
   {
     type: "category",
+    label: "Integrations",
     collapsible: false,
     collapsed: false,
-    label: "Overview",
     items: [
       {
         type: "doc",
         id: "integrations/overview",
         label: "Overview",
       },
-    ],
-  },
-  {
-    type: "category",
-    collapsible: false,
-    collapsed: false,
-    label: "LiveKit",
-    items: [
       {
         type: "doc",
         id: "integrations/livekit",
         label: "LiveKit",
       },
-    ],
-  },
-  {
-    type: "category",
-    collapsible: false,
-    collapsed: false,
-    label: "Pipecat",
-    items: [
       {
         type: "doc",
         id: "integrations/pipecat",
         label: "Pipecat",
       },
-    ],
-  },
-  {
-    type: "category",
-    collapsible: false,
-    collapsed: false,
-    label: "Vapi",
-    items: [
       {
         type: "doc",
         id: "integrations/vapi",
         label: "Vapi",
-      },
+      }
     ],
   },
 ] as const;
