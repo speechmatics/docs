2924667813809a1c963c08136577eb4be75cca88
lgavincrl <lucy.gavin@speechmatics.com>
2025-11-28 12:43:02 +0000

    integrations sidebar config update

--- docs/integrations/sidebar.ts
+++ docs/integrations/sidebar.ts
@@ -1,28 +1,54 @@
-export default {
-  type: "category",
-  label: "Integrations",
-  collapsible: false,
-  collapsed: false,
-  items: [
-    {
-      type: "doc",
-      id: "integrations/index",
-      label: "Introduction",
-    },
-    {
-      type: "doc",
-      id: "integrations/livekit",
-      label: "LiveKit",
-    },
-    {
-      type: "doc",
-      id: "integrations/pipecat",
-      label: "Pipecat",
-    },
-    {
-      type: "doc",
-      id: "integrations/vapi",
-      label: "Vapi",
-    },
-  ],
-} as const;
+export default [
+  {
+    type: "category",
+    collapsible: false,
+    collapsed: false,
+    label: "Overview",
+    items: [
+      {
+        type: "doc",
+        id: "integrations/overview",
+        label: "Overview",
+      },
+    ],
+  },
+  {
+    type: "category",
+    collapsible: false,
+    collapsed: false,
+    label: "LiveKit",
+    items: [
+      {
+        type: "doc",
+        id: "integrations/livekit",
+        label: "LiveKit",
+      },
+    ],
+  },
+  {
+    type: "category",
+    collapsible: false,
+    collapsed: false,
+    label: "Pipecat",
+    items: [
+      {
+        type: "doc",
+        id: "integrations/pipecat",
+        label: "Pipecat",
+      },
+    ],
+  },
+  {
+    type: "category",
+    collapsible: false,
+    collapsed: false,
+    label: "Vapi",
+    items: [
+      {
+        type: "doc",
+        id: "integrations/vapi",
+        label: "Vapi",
+      },
+    ],
+  },
+] as const;
