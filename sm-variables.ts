ef614054e2f354628b412928d140c3f656f18c54
Owais Aamir <aamirt@speechmatics.com>
2026-05-13 16:26:03 +0100

    STT helm chart version 1.3.0

--- sm-variables.ts
+++ sm-variables.ts
@@ -3,7 +3,7 @@ export const smVariables = {
   latestApplianceVersion: "6.3.0",
   latestContainerVersion: "15.7.0",
   usageContainerVersion: "0.3.0",
-  helmChartVersion: "1.2.0",
+  helmChartVersion: "1.3.0",
 } as const;
 
 // TODO investigate if there is a better way to handle these version numbers
