1cf72317c6cbbf2464571061d6ec33420fdc55f2
Matt Nemitz <mattn@speechmatics.com>
2026-05-13 21:55:32 +0100

    LLM discoverability: HTML directive to markdown (#251)

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,12 +1,19 @@
-import { Theme } from "@radix-ui/themes";
+import { Theme, VisuallyHidden } from "@radix-ui/themes";
 import MixpanelProvider from "@site/src/components/MixpanelProvider";
 import React from "react";
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
   return (
-    <Theme accentColor="cyan">
-      <MixpanelProvider>{children}</MixpanelProvider>
-    </Theme>
+    <>
+      <VisuallyHidden>
+        For AI agents: a documentation index is available at /llms.txt. Markdown
+        versions of all pages can be requested by appending `.md` to the URL, or
+        by setting the `Accept` header to `text/markdown`.
+      </VisuallyHidden>
+      <Theme accentColor="cyan">
+        <MixpanelProvider>{children}</MixpanelProvider>
+      </Theme>
+    </>
   );
 }
